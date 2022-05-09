10c2dddf98ddb7f2de0ec32615410d22a5c8d81a
Yetife <tifetreats21@gmail.com>
2022-05-09 23:20:25 +0100

    Cryto files

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import {Routes, Route, Link} from 'react-router-dom'
 import {Layout, Typography, Space} from 'antd'
 import './App.css'
 import {Navbar, Exchanges, Homepage, Cryptocurrencies, CryptoDetails} from "./components";
+import News from "./components/News";
 
 
 
@@ -20,14 +21,16 @@ const App = () => {
                        <Route path="/exchanges"  element={<Exchanges/>}/>
                        <Route path="/cryptocurrencies" element={<Cryptocurrencies/>}/>
                        <Route path="/crypto/:coinId" element={<CryptoDetails />} />
-                       {/*<Route path="/news" element={<News/>}/>*/}
+                       <Route path="/news" element={<News/>}/>
                    </Routes>
                </div>
            </Layout>
            <div className="footer" >
                 <Typography.Title level={5} style={{color: 'white', textAlign: 'center'}}>
-                    Cryptoverse <br/>
-                    All rights reserved
+                    <Link to="/">
+                        Cryptoverse Inc.
+                    </Link> <br />
+                    All Rights Reserved.
                 </Typography.Title>
                <Space>
                    <Link to="/">Home</Link>
